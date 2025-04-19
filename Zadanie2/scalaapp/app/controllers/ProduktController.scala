36e386982c30629b6031c47d7a956a6275cbd01e
WBZdeb <wojciech.zdeb98@gmail.com>
2025-04-19 16:57:34 +0200

    endpointy zgodnie z CRUD dla produktow

--- Zadanie2/scalaapp/app/controllers/ProduktController.scala
+++ Zadanie2/scalaapp/app/controllers/ProduktController.scala
@@ -2,8 +2,11 @@ package controllers
 
 import javax.inject._
 import play.api.mvc._
-import models.Produkt
+import models.{Produkt, ProduktForm}
 import scala.collection.mutable
+import play.api.data._
+import play.api.data.Forms._
+import play.api.data.format.Formats._
 
 @Singleton
 class ProduktController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
@@ -12,6 +15,21 @@ class ProduktController @Inject()(val controllerComponents: ControllerComponents
     Produkt(1, "Produkt A", 100.0),
     Produkt(2, "Produkt B", 150.0),
     Produkt(3, "Produkt C", 200.0)
+  )
+
+  val produktForm: Form[ProduktForm] = Form(
+    mapping(
+      "nazwa" -> nonEmptyText,
+      "cena" -> of[Double]
+    )(ProduktForm.apply)(ProduktForm.unapply)
+  )
+
+  val produktUpdateForm: Form[Produkt] = Form(
+    mapping(
+      "id" -> number,
+      "nazwa" -> nonEmptyText,
+      "cena" -> of[Double]
+    )(Produkt.apply)(Produkt.unapply)
   )
 
   // Endpoint Read All
@@ -27,4 +45,43 @@ class ProduktController @Inject()(val controllerComponents: ControllerComponents
     }
   }
 
+  // Endpoint Create
+  def createProdukt(): Action[AnyContent] = Action { implicit request =>
+    produktForm.bindFromRequest.fold(
+      errorForm => {
+        BadRequest("Blad w formularzu")
+      },
+      data => {
+        val nextId = if (produkty.isEmpty) 1 else produkty.map(_.id).max + 1
+        val produkt = Produkt(nextId, data.nazwa, data.cena)
+        produkty += produkt
+        Ok(s"Produkt zostal dodany.")
+      }
+    )
+  }
+
+  // Endpoint Update
+  def updateProdukt(): Action[AnyContent] = Action { implicit request =>
+    produktUpdateForm.bindFromRequest.fold(
+      errorForm => {
+        BadRequest("Blad w formularzu")
+      },
+      data => {
+        val produkt = Produkt(data.id, data.nazwa, data.cena)
+        produkty.update(data.id, produkt)
+        Ok(s"Produkt o id ${data.id} został zaktualizowany.")
+      }
+    )
+  }
+
+  // Endpoint Delete
+  def deleteProdukt(id: Int): Action[AnyContent] = Action {
+    val produktToDelete = produkty.find(_.id == id)
+    produktToDelete match {
+      case Some(produkt) =>
+        produkty -= produkt
+        Ok(s"Produkt o id $id został usunięty.")
+      case None => NotFound(s"Produkt o id $id nie znaleziony")
+    }
+  }
 }
---
+++ Zadanie2/scalaapp/app/models/ProduktForm.scala
@@ -0,0 +1,3 @@
+package models
+
+case class ProduktForm(nazwa: String, cena: Double)
